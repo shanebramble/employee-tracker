aee4febd60767aea02e41b532413740581d0de9b
Shane Bramble-Wade <sbramwade@msn.com>
2021-09-18 17:08:13 -0500

    finalized the queries for each prompt

--- app.js
+++ app.js
@@ -1,15 +1,5 @@
-const inquirer = require('inquirer');
 const db = require('./db/connection');
-const {
-    viewDepartment,
-    viewRoles,
-    viewEmployee,
-    addDepartment,
-    addRole,
-    addEmployee,
-    updateEmployeeRole,
-    Quit
-} = require('./lib/prompts');
+const {mainPrompts} = require('./lib/prompts');
 
 // Initialize the beginning prompts.
 function init() {
@@ -19,47 +9,6 @@ function init() {
 
     mainPrompts();
 }
-//  A prompt showcasing a list of different options.
-function mainPrompts() {
-    inquirer
-        .prompt([{
-            type: "list",
-            name: "option",
-            message: "What would you like to do?",
-            choices: ['view all departments', 'view all roles', 'view all employees', 'add a department', 'add a role', 'add an employee', 'update an employee role', 'Quit']
-        }])
-        .then((ans) => {
-            let selectedOption = ans.option;
-            // Based on selection we trigger certain functions.
-            switch (selectedOption) {
-                case 'view all departments':
-                    viewDepartment();
-                    break;
-                case 'view all roles':
-                    viewRoles();
-                    break;
-                case 'view all employees':
-                    viewEmployee();
-                    break;
-                case 'add a department':
-                    addDepartment();
-                    break;
-                case 'add a role':
-                    addRole();
-                    break;
-                case 'add an employee':
-                    addEmployee();
-                    break;
-                case 'update an employee role':
-                    updateEmployeeRole();
-                    break;
-                default:
-                    Quit();
-                    break;
-            }
-        });
-}
-
 // Start the original prompt listings.
 init();
 
